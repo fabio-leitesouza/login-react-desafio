c82c0ae3552e50fbb1eca2ac79c31f150c54a5f3
Fabio Roberto Leite Souza <11095@senacminas.edu.br>
2023-02-28 13:59:55 -0300

    form app alter

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <img src="/images/logo.svg" alt="logo" className="login-text__logo" />
-      <Form />
+      <div className="side-left">
+        <img src="/images/logo.svg" alt="logo" className="login-text__logo" />
+        <Form />
+      </div>
       
+      <img src="/images/side-image.jpg" alt="slide imagem" className="login-text__side" />
     </div>
   );
 }
